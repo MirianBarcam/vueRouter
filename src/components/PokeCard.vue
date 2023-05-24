ab53f448dec5ea550a49f75b135b1467c3f68970
Mirian Barajas <mirianbarcam@gmail.com>
2023-05-24 22:26:56 +0000

    card favorites

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,8 +1,50 @@
-<script setup></script>
+<script setup>
+const props = defineProps(['imgPoke', '', 'namePoke'])
+//const emit = defineEmits(['prevPage', 'nextPage'])
+
+/*const activarsiguiente = () => {
+  emit('nextPage')
+}*/
+</script>
 
 <template>
   <div class="card-container">
-    <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png" />
-    <h1>Name</h1>
+    <div class="card-container-wrapper-img">
+      <img :src="imgPoke" class="card-container-img" />
+    </div>
+
+    <h3>{{ namePoke }}</h3>
   </div>
 </template>
+<style>
+.card-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid grey;
+  width: 30rem;
+  height: 5rem;
+  border-radius: 20px;
+  padding: 1rem;
+  gap: 2rem;
+  overflow: hidden;
+  background-color: rgba(191, 199, 195, 0.355);
+}
+.card-container:hover {
+  transition: 0.5s;
+  width: 30.2rem;
+  height: 5.2rem;
+}
+@supports (object-fit: cover) {
+  .card-container-wrapper-img {
+    width: 3rem;
+    height: 100%;
+    object-fit: cover;
+    object-position: center center;
+  }
+}
+.card-container-img {
+  width: 100%;
+  height: auto;
+}
+</style>
--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -4,7 +4,10 @@ import PokeCard from '../components/PokeCard.vue'
 <template>
   <div>
     <h1>This is an favorites page</h1>
-    <PokeCard></PokeCard>
+    <PokeCard
+      :name-poke="'pikachu'"
+      :img-poke="'https://www.bitgab.com/uploads/1597796080-pikachu-1597796080.png'"
+    ></PokeCard>
   </div>
 </template>
 
